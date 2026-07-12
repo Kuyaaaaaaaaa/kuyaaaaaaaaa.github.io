22ec8d8c32c34aa3f7554651b204afe9ea44e4d8
Kuyaaaaaaaaa <cornicksforever837@gmail.com>
2026-07-12 17:54:59 -0500

    Modify player object structure and energy value
    Updated player object to include compressed energy and cost, and changed energy value.

--- js/player.js
+++ js/player.js
@@ -4,12 +4,19 @@
 
 
 const player = {
-    energy: new Decimal(120.9),
+    energy: new Decimal(4e20),
     // hehehe
     dimensions: createAllDimensions(),
+    compressedEnergy: new Decimal(0),
+    compressedEnergyCost: new Decimal(1e18),
+    entropy: new Decimal(0),
     notation: "Mixed Scientific",
-    version: 1
+    version: 2
 };
+
+const achievements = [
+    createAllAchievements()
+];
 
 
 const deltaTime = 0.05;
